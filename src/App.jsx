cc902585263f4221b2340ddcb0e8753fdf88e44a
Noel Anthony <noelanthony120@gmail.com>
2023-02-28 00:02:26 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+     <>
+     <img src="src\images\reactjs-icon.png" />
+     <h1>ReactFacts</h1>
+     <h4>React Course - Project 1</h4>
+     </>   
+    )
+}
