d2b3e04fa154501cb1e114f3a497c253286271f2
Noel Anthony <noelanthony120@gmail.com>
2023-02-28 18:24:29 +0800

    finish

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 export default function Navbar() {
     return (
-     <>
-     <img src="src\images\reactjs-icon.png" />
-     <h1>ReactFacts</h1>
-     <h4>React Course - Project 1</h4>
-     </>   
+     <nav className="navbar">
+        {/* <img src="src\images\reactjs-icon.png" /> */}
+        <img src="src\images\reactjs-icon.png"  className="nav--icon"/>
+        <h3 className="nav--logo_text">ReactFacts</h3>
+        <h4 className="nav--title">React Course - Project 1</h4>
+     </nav>   
     )
 }
